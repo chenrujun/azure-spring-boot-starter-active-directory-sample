0289ca9ce63621ae3b7754a46a43ba577555373a
Rujun Chen <rujche@microsoft.com>
2020-11-10 14:48:51 +0800

    Use space instead of tab.

--- src/main/java/chenrujun/azure/spring/boot/starter/active/directory/sample/AzureSpringBootStarterActiveDirectorySampleApplication.java
+++ src/main/java/chenrujun/azure/spring/boot/starter/active/directory/sample/AzureSpringBootStarterActiveDirectorySampleApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AzureSpringBootStarterActiveDirectorySampleApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AzureSpringBootStarterActiveDirectorySampleApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AzureSpringBootStarterActiveDirectorySampleApplication.class, args);
+    }
 
 }
--- src/test/java/chenrujun/azure/spring/boot/starter/active/directory/sample/AzureSpringBootStarterActiveDirectorySampleApplicationTests.java
+++ src/test/java/chenrujun/azure/spring/boot/starter/active/directory/sample/AzureSpringBootStarterActiveDirectorySampleApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class AzureSpringBootStarterActiveDirectorySampleApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
