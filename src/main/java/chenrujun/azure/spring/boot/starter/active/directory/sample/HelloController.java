b32e7c0d2d33a9b5333336be4e3d08f12b5cf8af
Rujun Chen <rujche@microsoft.com>
2020-11-11 08:33:19 +0800

    Output the information of accessToken and refreshToken.

--- src/main/java/chenrujun/azure/spring/boot/starter/active/directory/sample/HelloController.java
+++ src/main/java/chenrujun/azure/spring/boot/starter/active/directory/sample/HelloController.java
@@ -1,25 +1,77 @@
 package chenrujun.azure.spring.boot.starter.active.directory.sample;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.core.AbstractOAuth2Token;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 @RestController
-@RequestMapping("/hello")
 public class HelloController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(HelloController.class);
 
-    @GetMapping
-    public String hello(@RegisteredOAuth2AuthorizedClient("graph") OAuth2AuthorizedClient client) {
-        StringBuilder builder = new StringBuilder();
-        builder.append("hello");
-        builder.append("\r\n");
-        builder.append("access token: " + client.getAccessToken().getTokenValue());
-        builder.append("\r\n");
-        builder.append("refresh token: " + client.getRefreshToken().getTokenValue());
-        builder.append("\r\n");
+    @GetMapping("/graph")
+    public String graph(
+        @RegisteredOAuth2AuthorizedClient("graph") OAuth2AuthorizedClient oAuth2AuthorizedClient
+    ) {
+        logAuthorizedClient(oAuth2AuthorizedClient);
+        return "graph";
 
-        return  builder.toString();
+    }
+
+    @GetMapping("/arm")
+    public String arm(
+        @RegisteredOAuth2AuthorizedClient("arm") OAuth2AuthorizedClient oAuth2AuthorizedClient
+    ) {
+        logAuthorizedClient(oAuth2AuthorizedClient);
+        return "arm";
+
+    }
+
+    static void logAuthorizedClient(OAuth2AuthorizedClient authorizedClient) {
+        LOGGER.info(
+            "principalName = {} ---------------------------",
+            authorizedClient.getPrincipalName()
+        );
+        LOGGER.info(
+            "clientName = {}",
+            Optional.of(authorizedClient)
+                    .map(OAuth2AuthorizedClient::getClientRegistration)
+                    .map(ClientRegistration::getClientName)
+                    .orElse(null)
+        );
+        LOGGER.info(
+            "accessTokenIssuedAt = {}",
+            Optional.of(authorizedClient)
+                    .map(OAuth2AuthorizedClient::getAccessToken)
+                    .map(AbstractOAuth2Token::getIssuedAt)
+                    .orElse(null)
+        );
+        LOGGER.info(
+            "accessTokenExpiresAt = {}",
+            Optional.of(authorizedClient)
+                    .map(OAuth2AuthorizedClient::getAccessToken)
+                    .map(AbstractOAuth2Token::getExpiresAt)
+                    .orElse(null)
+        );
+        LOGGER.info(
+            "refreshTokenIssuedAt = {}",
+            Optional.of(authorizedClient)
+                    .map(OAuth2AuthorizedClient::getRefreshToken)
+                    .map(AbstractOAuth2Token::getIssuedAt)
+                    .orElse(null)
+        );
+        LOGGER.info(
+            "refreshTokenExpiresAt = {}",
+            Optional.of(authorizedClient)
+                    .map(OAuth2AuthorizedClient::getRefreshToken)
+                    .map(AbstractOAuth2Token::getExpiresAt)
+                    .orElse(null)
+        );
     }
 }
